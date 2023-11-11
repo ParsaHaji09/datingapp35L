d2c17253c66663cbf055e1ac275b60c74740d136
Larry Zhi <larryzhi@Larrys-MacBook-Pro.local>
2023-11-10 16:09:58 -0800

    set up components, assests folder and routes

--- datingapp/src/App.js
+++ datingapp/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+import "./index.css";
+import "./App.css";
+import "./bootstrap.min.css";
 import './App.css';
+
+import Login from './Components/Auth/Login'
+import Register from './Components/Auth/Register'
+import Explore from './Components/Explore/Explore'
+import Profile from './Components/Profile/Profile'
+import Rating from "./Components/Rating/Rating";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/rating" element={<Rating />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+    </Router>
+    
   );
 }
 
