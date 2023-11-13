b7cfdc4a8db959d7f6d30e0e0ca9aa9955b52e1c
Parsa Hajipour <47697224+ParsaHaji09@users.noreply.github.com>
2023-11-12 16:16:25 -0800

    Basic Search Bar with Example Country List
    Made Search Bar to search for usernames. Does not have "real" data yet and does not connect to database yet.

--- datingapp/src/Components/Explore/Explore.js
+++ datingapp/src/Components/Explore/Explore.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react'
+const Explore = () => {
 
-function Explore() {
-  return (
-    <div>
-      <h1>Explore!</h1>
-    </div>
-  );
+ const [searchInput, setSearchInput] = useState("");
+
+ const countries = [
+
+  { name: "Belgium", continent: "Europe" },
+  { name: "India", continent: "Asia" },
+  { name: "Bolivia", continent: "South America" },
+  { name: "Ghana", continent: "Africa" },
+  { name: "Japan", continent: "Asia" },
+  { name: "Canada", continent: "North America" },
+  { name: "New Zealand", continent: "Australasia" },
+  { name: "Italy", continent: "Europe" },
+  { name: "South Africa", continent: "Africa" },
+  { name: "China", continent: "Asia" },
+  { name: "Paraguay", continent: "South America" },
+  { name: "Usa", continent: "North America" },
+  { name: "France", continent: "Europe" },
+  { name: "Botswana", continent: "Africa" },
+  { name: "Spain", continent: "Europe" },
+  { name: "Senegal", continent: "Africa" },
+  { name: "Brazil", continent: "South America" },
+  { name: "Denmark", continent: "Europe" },
+  { name: "Mexico", continent: "South America" },
+  { name: "Australia", continent: "Australasia" },
+  { name: "Tanzania", continent: "Africa" },
+  { name: "Bangladesh", continent: "Asia" },
+  { name: "Portugal", continent: "Europe" },
+  { name: "Pakistan", continent: "Asia" },
+
+];
+
+const getFilteredItems = (input, itemList) => {
+  if(!input){
+    return itemList;
+  }
+  return itemList.filter(country => country.name.toLowerCase().includes(input.toLowerCase()))
 }
+
+const filteredCountries = getFilteredItems(searchInput, countries);
+
+return <div>
+
+<input type="search" placeholder="Search here" onChange={e => setSearchInput(e.target.value)} value={searchInput} />
+
+<ul>
+  {filteredCountries.map(value => <p>{value.name}</p>)}
+</ul>
+
+</div>
+
+
+};
 
 export default Explore;
