caec2ede420a053966120bed7c9db7b4e0851449
Parsa Hajipour <47697224+ParsaHaji09@users.noreply.github.com>
2023-12-02 13:26:13 -0800

    Search Bar CSS/Basic Alg
    Improved outlook of search bar, and now can search for tags and usernames

--- datingapp/src/Components/Explore/Explore.js
+++ datingapp/src/Components/Explore/Explore.js
@@ -1,36 +1,149 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { BsSearch} from "react-icons/bs"
-import '../../App.css'
+import { BsSearch, BsStarFill} from "react-icons/bs"
+import './Explore.css'
 
 const Explore = () => {
 
+  const users = [
+    {
+      _id: "1",
+      name: "Bob Marley",
+      tags:
+        ["funny", "smart", "artistic", "wholesome"],
+      rating: 9.7,
+    },
+    {
+      _id: "2",
+      name: "Joe Dahmer",
+      tags: ["friendly", "cool", "fun"],
+      rating: 0.2,
+    },
+    {
+        _id: "3",
+        name: "Archduke Ferdinand",
+        tags: ["powerful", "commanding", "charismatic", "important"],
+        rating: 6.2,
+    },
+    {
+        _id: "4",
+        name: "Bonobo Chimp",
+        tags: ["Best", "Monkey", "Friendly"],
+        rating: 10.0,
+    },
+    {
+      _id: 5,
+      name: "Alice Wonderland",
+      tags: ["funny", "smart", "artistic", "wholesome"],
+      rating: 8.5
+    },
+    {
+      _id: 6,
+      name: "Eva Einstein",
+      tags: ["friendly", "cool", "fun"],
+      rating: 7.9
+    },
+    {
+      _id: 7,
+      name: "Queen Victoria",
+      tags: ["powerful", "commanding", "charismatic", "important"],
+      rating: 9.1
+    },
+    {
+      _id: 8,
+      name: "Charlie Chimpanzee",
+      tags: ["Best", "Monkey", "Friendly"],
+      rating: 9.8
+    },
+    {
+      _id: 9,
+      name: "David Bowie",
+      tags: ["funny", "smart", "artistic", "wholesome"],
+      rating: 8.7
+    }
+    
+  ];
+
   const [searchInput, setSearchInput] = useState("");
   const [searchResult, setSearchResult] = useState([]);
+  const [error, setError] = useState(false);
 
-  useEffect(() => {
-    const search = async () => {
-      try{
-        if (!searchInput){
-          setSearchResult([])
-          return
+  const handleSearchClick = async (e) => {
+    e.preventDefault()
+    console.log(searchInput)
+    console.log(users)
+
+    const searchTerm = searchInput.toLowerCase();
+
+    // Filter the users array based on the searchInput
+    const matchingUsers = users.filter(user => {
+      // Check if any property of the user matches the search term
+      return Object.values(user).some(value => {
+        if (Array.isArray(value)) {
+          // If the property is an array, check if any element matches
+          return value.some(tag => tag.toLowerCase().includes(searchTerm));
+        } else if (typeof value === 'string') {
+          // If the property is a string, check if it includes the search term
+          return value.toLowerCase().includes(searchTerm);
+        } else {
+          // For other types of properties, convert to string and check
+          return String(value).toLowerCase().includes(searchTerm);
         }
-        const result = await axios.get("/api/users/all-users", {params: {key: searchInput, limit: 5}}) // ask which api endpoint to get
-        setSearchInput(result.data.data)
-        console.log(result)
-      }
-      catch (error){
-        console.log(error)
-      }
-    }
-    search()
-  }, [searchInput])
+      });
+    });
+
+    const sortedUsers = matchingUsers.sort((a, b) => b.rating - a.rating);
+    
+
+    //sorted_list = []
+    setSearchResult(sortedUsers)
+
+    //go into db
+  //   try{
+  //     const config = {
+  //       headers: {
+  //         "Content-type":"application/json",
+  //       },
+  //     };
+      
+  //     const regData = await axios.get("/api/users/all-users", config);
+
+  //     console.log(regData)
+  //     console.log(regData.data.length);
+  //     console.log("Endpoint reached");
+      
+
+  //   } catch (error) {
+  //     setError(error.message);
+  //     console.log(error);
+  //   }
+  }
+
+    
+    
+  // useEffect(() => {
+  //   const search = async () => {
+  //     try{
+  //       if (!searchInput){
+  //         setSearchResult([])
+  //         return
+  //       }
+  //       const result = await axios.get("/api/users/all-users", {params: {key: searchInput, limit: 5}})
+  //       setSearchInput(result.data.data)
+  //       console.log(result)
+  //     }
+  //     catch (error){
+  //       console.log(error)
+  //     }
+  //   }
+  //   search()
+   //}, [searchInput])
+
   return (
     <form>
-      <div className='explore-wrapper'>
-        <div className='form-group'>
-          <button className = 'search-btn'><BsSearch/></button>
+      <div className='explore-all-wrapper'>
+        <div className='search-wrapper'>
+          
           <input
             className='search-input'
             type = "text"
@@ -38,12 +151,21 @@ const Explore = () => {
             value = {searchInput}
             onChange={e => setSearchInput(e.target.value)}
           />
+          <button className = 'search-btn' onClick={handleSearchClick}><BsSearch/></button>
         </div>
         {searchResult && searchResult.length > 0 && (
           <div className='search-result'>
             {searchResult.map(user => (
                 <div className='result-item' key = {user._id}>
-                  <p className='name'>{user.name}</p>
+                    <div className='name-tags'>
+                      <button class="name">{user.name}</button>
+                      <span class= "tags">{user.tags.join(", ")}</span>
+                    </div>
+                    <div className='star-rating'>
+                      <span class= "star"><BsStarFill/></span>
+                      <span class= "rating">{user.rating}</span>
+                    </div>
+                  
                 </div>
               ),
             )}
